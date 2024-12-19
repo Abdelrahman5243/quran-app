6e27dad8bde557b6c7f4cf89e748afb536b8cc28
Abdelrahman Ragab <159382106+Abdelrahman5243@users.noreply.github.com>
2024-12-19 11:19:45 +0000

    add reset btn and stopPropagation

--- src/components/AthkarCard/AthkarCard.jsx
+++ src/components/AthkarCard/AthkarCard.jsx
@@ -9,6 +9,11 @@ const AthkarCard = ({ content, count }) => {
     }
   }, [currentCount]);
 
+  const countReset = useCallback((event) => {
+    event.stopPropagation(); // Prevent event bubbling
+    setCurrentCount(count);
+  }, [count]);
+
   const handleKeyDown = useCallback(
     (event) => {
       if (event.key === " " || event.key === "Enter") {
@@ -20,12 +25,13 @@ const AthkarCard = ({ content, count }) => {
   );
 
   const cardClasses = useMemo(
-    () => `flex justify-between items-center p-4 bg-light-2 dark:bg-dark-2 rounded-lg cursor-pointer transition-colors duration-300 hover:bg-light-1 
-    dark:hover:bg-dark-1 outline-none ${
-      currentCount === 0
-        ? "border-2 border-green-500"
-        : "border-2 dark:border-dark-2 border-light-2"
-    }`,
+    () =>
+      `flex justify-between items-center gap-8 p-4 bg-light-2 dark:bg-dark-2 rounded-lg cursor-pointer transition-colors duration-300 hover:bg-light-1 
+      dark:hover:bg-dark-1 outline-none ${
+        currentCount === 0
+          ? "border-2 border-green-500"
+          : "border-2 dark:border-dark-2 border-light-2"
+      }`,
     [currentCount]
   );
 
@@ -35,11 +41,28 @@ const AthkarCard = ({ content, count }) => {
       onKeyDown={handleKeyDown}
       className={cardClasses}
       tabIndex={0}
+      aria-label="Athkar card"
     >
       <p className="text-dark-1 dark:text-light-1">{content}</p>
-      <p className="text-dark-1 dark:text-light-1 font-bold text-xl px-4 select-none">
-        {currentCount}
-      </p>
+      <div className="flex flex-col gap-4 items-center bg-light-1 dark:bg-dark-1 p-2 rounded min-w-24">
+        <p className="text-dark-1 dark:text-light-1 font-bold text-xl px-4 select-none">
+          {currentCount}
+        </p>
+        <div className="flex justify-between w-full items-center">
+          <p className="text-dark-1 dark:text-light-1 opacity-20 font-bold text-xl select-none">
+            {count}
+          </p>
+          <button
+            onClick={countReset}
+            onKeyDown={(e) => e.key === "Enter" && countReset(e)}
+            tabIndex={0}
+            aria-label="Reset counter"
+            className="text-dark-1 dark:text-light-1 opacity-75 hover:opacity-100 focus:outline-none"
+          >
+            <i className="ri-arrow-go-forward-line"></i>
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
